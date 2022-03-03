25d0c3eec493ac53f0ff65a795cff2f4057a435b
Shraddha <mailmeatshraddha@gmail.com>
2022-03-03 17:33:28 -0500

    feat: add pricing card

---
+++ src/components/PricingCard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function PricingCard(){
+  return (
+    <article className="price-card">
+      <p className="text-center">10–50 orders / day</p>
+      <div className="price-card-down-arrow"></div>
+      <p className="text-center">$500–$1000 / month</p>
+    </article>
+  );
+}
+
+export default PricingCard;
