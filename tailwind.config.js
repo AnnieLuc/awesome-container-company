42075c8441f40fa88710969f1a3edea1dffc9561
Shraddha <mailmeatshraddha@gmail.com>
2022-03-02 22:27:51 -0500

    feat: setup tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,39 @@
+module.exports = {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}',
+    'public/**/*.html',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#1c383',
+        secondary: {
+          100: '#ffffff',
+          200: '#f4f4f4',
+        },
+        highlight: '#ff9820',
+      },
+      fontFamily: {
+        serif: ['Spline Sans', 'sans-serif']
+      },
+      fontSize: {
+        xs: '12px',
+        sm: '14px',
+        base: '18px',
+        lg: '24px',
+        xl: '44px'
+      },
+      lineHeight: {
+        '3': '14.22px',
+        '4': '16.59px',
+        '5': '21.33px',
+        '6': '24px',
+        '7': '28.44px',
+        '8': '30px',
+        '9': '48px',
+        '10': '52.14px'
+      }
+    },
+  },
+  plugins: [],
+}
