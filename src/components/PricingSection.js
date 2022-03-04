b56979bda77411b571a7f761ee58c19a0a05fad1
Shraddha <mailmeatshraddha@gmail.com>
2022-03-04 15:22:20 -0500

    feat: add reusable button component

--- src/components/PricingSection.js
+++ src/components/PricingSection.js
@@ -1,17 +1,19 @@
 import React            from 'react';
 import SectionHeading   from './SectionHeading';
 import PricingCard      from './PricingCard';
+import Button           from './Button';
 import pricing          from '../utils/pricing';
 
 function PricingSection(){
 
   return (
     <section>
-      <div className="mx-auto px-[82px] max-w-[1440px]">
+      <div className="mx-auto px-[82px] max-w-[1440px] flex flex-col items-center">
         <SectionHeading align="center" color="primary">Our pricing</SectionHeading>
-        <div className=" mt-[60px] mb-[80px] grid grid-cols-4 gap-[40px]">
-          {pricing.map(price => <PricingCard {...price} />)}
+        <div className="mt-[60px] mb-[80px] grid grid-cols-4 gap-[40px]">
+          {pricing.map((price, index) => <PricingCard key={index} {...price} />)}
         </div>
+        <Button>Work with us</Button>
       </div>
     </section>
   );
--- src/components/WorkWithUsButton.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function WorkWithUsButton(){
-
-}
-
-export default WorkWithUsButton;
