85a242df211288fe3d7c39a2250f209dcfe3fe6a
Shraddha <mailmeatshraddha@gmail.com>
2022-03-04 16:48:11 -0500

    fix: add missing box-shadow

--- tailwind.config.js
+++ tailwind.config.js
@@ -36,6 +36,9 @@ module.exports = {
         '9': '48px',
         '10': '52.14px'
       },
+      boxShadow: {
+        DEFAULT: '0px 2px rgba(0, 0, 0, 0.2)',
+      }
     },
   },
   plugins: [],
