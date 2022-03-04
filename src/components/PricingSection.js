f51747fe892380f3f9df38c8fa6648b1b5175943
Shraddha <mailmeatshraddha@gmail.com>
2022-03-04 14:51:42 -0500

    feat: make price card a reusable component

--- src/components/PricingSection.js
+++ src/components/PricingSection.js
@@ -1,13 +1,18 @@
 import React            from 'react';
 import SectionHeading   from './SectionHeading';
 import PricingCard      from './PricingCard';
-
+import pricing          from '../utils/pricing';
 
 function PricingSection(){
+
   return (
     <section>
-      <SectionHeading align="center" color="primary">Our pricing</SectionHeading>
-      <PricingCard />
+      <div className="mx-auto px-[82px] max-w-[1440px]">
+        <SectionHeading align="center" color="primary">Our pricing</SectionHeading>
+        <div className=" mt-[60px] mb-[80px] grid grid-cols-4 gap-[40px]">
+          {pricing.map(price => <PricingCard {...price} />)}
+        </div>
+      </div>
     </section>
   );
 }
