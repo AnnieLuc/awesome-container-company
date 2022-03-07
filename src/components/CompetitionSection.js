7cc921a3bd8fd081c03a9b6e67d51658fce66ffc
Shraddha <mailmeatshraddha@gmail.com>
2022-03-07 12:31:18 -0500

    fix: fix the layout

--- src/components/CompetitionSection.js
+++ src/components/CompetitionSection.js
@@ -1,13 +1,15 @@
-import React              from 'react';
-import SectionHeading     from './SectionHeading';
-import CompetitionTable   from './CompetitionTable';
+import React from 'react';
+import SectionHeading from './SectionHeading';
+import CompetitionTable from './CompetitionTable';
 
 function CompetitionSection() {
   return (
-    <section className="py-[120px] bg-secondary-200">
+    <section className=" bg-secondary-200">
+      <div className="mx-auto py-[120px] max-w-[1440px] flex flex-col items-center">
         <SectionHeading align="center" color="primary">The competition is no match</SectionHeading>
-      <div className="pt-[60px]">
-        <CompetitionTable />
+        <div className="pt-[60px]">
+          <CompetitionTable />
+        </div>
       </div>
     </section>
   );
