b55bfa59810541cca497bb233f091ea67b1df33c
Alec <drosualec@gmail.com>
2022-03-07 17:35:22 -0500

    fix: better semantic HTML tags & better formatting

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,66 +6,64 @@ import Twitter from '../images/social-links/twitter.svg';
 function Footer() {
 	return (
 		<footer className='bg-primary text-secondary-100 pt-[80px] pb-[60px]'>
-			<div className='flex justify-between max-w-[1440px] mx-auto border-rose-500 px-[80px]'>
+			<div className='flex justify-between max-w-[1440px] mx-auto px-[80px]'>
 				<div className=' flex flex-col'>
 					<SectionHeading align='left' color='secondary-100'>
 						Let's get in touch
 					</SectionHeading>
 					<div className='flex flex-col pt-[40px]'>
-						<span className='pb-[10px]'>
-							hello@acc.com <br />
-						</span>
-						+65 0123 4567 <br />
-						<br />
-						<br />
-						<p className='text-sm'>
-							Blk 35 Mandalay Road # 13-37 <br />
-							Mandalay Towers <br />
-							Singapore 308215
-						</p>
+						<span className='pb-[10px]'>hello@acc.com</span>
+						<span className='pb-[40px]'>+65 0123 4567</span>
+						<address className='text-sm not-italic'>
+							<span className='block'>Blk 35 Mandalay Road # 13-37</span>
+							<span className='block'>Mandalay Towers</span>
+							<span className='block'>Singapore 308215</span>
+						</address>
 					</div>
 				</div>
 				<div className='flex flex-col'>
 					<SectionHeading align='left' color='secondary-100'>
 						Follow us
 					</SectionHeading>
-					<div className='pt-[40px]'>
-						<a
-							href='https://www.instagram.com/tailwing/'
-							className='flex flex-row mb-[20px]'
-						>
-							<img
-								src={Instagram}
-								alt='instagram'
-								className='object-contain pr-[12px]'
-							/>
-							instagram
-						</a>
-						<a
-							href='https://www.twitter.com/tailwing/'
-							className='flex flex-row'
-						>
-							<img
-								src={Twitter}
-								alt='twitter'
-								className='object-contain pr-[12px]'
-							/>
-							twitter
-						</a>
-					</div>
+					<ul className='pt-[40px]'>
+						<li>
+							<a
+								href='https://www.instagram.com/tailwing/'
+								className='flex flex-row mb-[20px]'
+							>
+								<img
+									src={Instagram}
+									alt='instagram'
+									className='object-contain pr-[12px]'
+								/>
+								instagram
+							</a>
+						</li>
+						<li>
+							<a
+								href='https://www.twitter.com/tailwing/'
+								className='flex flex-row'
+							>
+								<img
+									src={Twitter}
+									alt='twitter'
+									className='object-contain pr-[12px]'
+								/>
+								twitter
+							</a>
+						</li>
+					</ul>
 				</div>
 				<div className='flex flex-col'>
 					<SectionHeading align='left' color='secondary-100'>
 						Where we work
 					</SectionHeading>
 					<div className='flex flex-col pt-[40px]'>
-						We currently operate in: <br />
-						Singapore <br />
-						<br />
-						<br />
-						Planning to launch in 2022: <br />
-						UK, EU, OSEAN, China, Vietnam, Malaysia, <br />
-						Taiwan
+						<span>We currently operate in:</span>
+						<span className='pb-[40px]'>Singapore</span>
+						<span>Planning to launch in 2022:</span>
+						<span>UK, EU, OSEAN, China, Vietnam, Malaysia,</span>
+						<span>Taiwan</span>
 					</div>
 				</div>
 			</div>
