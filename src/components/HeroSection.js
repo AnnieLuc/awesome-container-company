639664e7a0612bfae9bf59fd45eab99c033bf15e
Shraddha <mailmeatshraddha@gmail.com>
2022-03-09 12:34:09 -0500

    feat: add slider caption

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -15,6 +15,10 @@ function HeroSection() {
         <div className="mt-[85px] w-full flex justify-between">
           <div className="max-w-[626px] w-full rounded-[20px] object-cover">
             <ImageCompareSlider />
+            <div className="mt-[13px] w-full flex justify-between text-sm font-normal leading-4">
+                <p>Now</p>
+                <p>With Awesome Containers</p>
+            </div>
           </div>
           <div className="max-w-[491px] w-full">
             <Chat />
