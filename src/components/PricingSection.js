ac30c02bb20e63613b116dff1dd0b54d356e1145
Shraddha <mailmeatshraddha@gmail.com>
2022-03-11 13:41:30 -0500

    feat: update the pricing section

--- src/components/PricingSection.js
+++ src/components/PricingSection.js
@@ -1,18 +1,26 @@
 import React            from 'react';
 import SectionHeading   from './SectionHeading';
 import SectionPara      from './SectionPara';
-import PricingCard      from './PricingCard';
 import Button           from './Button';
-import pricing          from '../utils/pricing';
+import pricingContainer from '../images/pricing-section-container.svg';
 
 function PricingSection(){
 
   return (
     <section>
       <div className="mx-auto px-[82px] max-w-[1440px] flex flex-col items-center">
-        <SectionHeading align="center" color="primary">Our pricing</SectionHeading>
-        <div className="mt-[60px] mb-[80px] grid grid-cols-4 gap-[40px]">
-          {pricing.map((price, index) => <PricingCard key={index} {...price} />)}
+        <div className="mx-auto w-full max-w-[800px]">
+          <SectionHeading align="center" color="primary">Get in touch with us to calculate prices for your business</SectionHeading>
+        </div>
+        <div className="my-[92px] w-full max-w-[738px] flex">
+          <div className="w-full max-w-[195px] pt-[100px]">
+            <img src={pricingContainer} className="object-cover object-center" alt="pricing container" />
+          </div>
+          <div className="ml-[31px] px-[42px] py-[37px] w-full h-[158px] bg-[url('./images/pricing-section-callout.svg')] bg-no-repeat bg-center bg-cover flex justify-center items-center">
+            <p className="text-[20px] font-normal leading-[23.7px] text-center">
+              We calculate our prices according to the amount of containers and deliveries made by your business monthly.
+            </p>
+          </div>
         </div>
         <Button isHeader="false">Work with us</Button>
         <div className="mt-[32px] max-w-[618px]">
