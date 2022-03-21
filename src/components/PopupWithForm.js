e5bc59497ace85a7c24ca2763fbafeded99daba2
Alec Drosu Laptop <drosualec@gmail.com>
2022-03-21 14:53:21 -0600

    NON-functional code needed for demonstration

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -9,12 +9,26 @@ function PopupWithForm(props) {
     formState: { errors, isValid },
   } = useForm({ mode: "onChange" });
   const onSubmit = (data) => console.log(data);
+  // when onSubmit occurs, change the company name to entry.112, and change the email to entry.113
+  // const onSubmit = (data: formInputs) => {
+  //   props.onSubmit(data);
+  // };
 
   return (
     <section className={`form-popup ${props.isOpen ? "form-open" : ""}`}>
+      <iframe
+        title="hidden_iframe"
+        name="hidden_iframe"
+        id="hidden_iframe"
+        className="hidden"
+        method="POST"
+      ></iframe>
       <form
+        target="hidden_iframe"
+        action="https://docs.google.com/forms/u/0/d/e/1FAIpQLSe39Aky_ezYNI2bUtVcApgbw0qsJuSahyySi62W8WNo3ec6Xg/formResponse"
         className="w-[760px] h-[778px] bg-white px-[80px] rounded-[3px] shadow-sm"
-        onSubmit={handleSubmit(onSubmit)}
+        // onSubmit={handleSubmit(onSubmit)}
+        method="POST"
       >
         <button
           type="button"
@@ -36,6 +50,7 @@ function PopupWithForm(props) {
               : "Personal or company name *"}
           </div>
           <label
+            name="entry.1377521493"
             className={`h-[60px] ${
               errors.company ? "labels-invalid" : "labels"
             }`}
@@ -43,6 +58,7 @@ function PopupWithForm(props) {
             <input
               type="text"
               className="inputs"
+              name="entry.1377521493"
               {...register("company", { required: true })}
             />
           </label>
@@ -54,11 +70,13 @@ function PopupWithForm(props) {
             {errors.email ? "Email * (This field is required)" : "Email *"}
           </div>
           <label
+            name="entry.1586441139"
             className={`h-[60px] ${errors.email ? "labels-invalid" : "labels"}`}
           >
             <input
               type="email"
               className="inputs"
+              name="entry.1586441139"
               {...register("email", {
                 required: true,
                 pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
@@ -68,19 +86,23 @@ function PopupWithForm(props) {
           <div className="text-sm pb-[11px] text-primary">
             Your request/interest
           </div>
-          <label className="labels h-[180px]">
-            <textarea type="text" className="inputs h-[140px] resize-none" />
+          <label name="entry.401400152" className="labels h-[180px]">
+            <textarea
+              name="entry.401400152"
+              type="text"
+              className="inputs h-[140px] resize-none"
+            />
           </label>
         </div>
-        <Link to="/thanks">
-          <button
-            type="submit"
-            className={`${!isValid ? "button-invalid" : "button-form"}`}
-            disabled={!isValid}
-          >
-            Send
-          </button>
-        </Link>
+
+        <button
+          type="submit"
+          className={`${!isValid ? "button-invalid" : "button-form"}`}
+          disabled={!isValid}
+          onSubmit={handleSubmit(onSubmit)}
+        >
+          Send
+        </button>
       </form>
     </section>
   );
