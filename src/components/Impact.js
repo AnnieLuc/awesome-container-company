c4f7140391d96df6f96a54767bc59d20d14fb098
Ekaterina Cratcha <ekaterina.cratcha@yandex.com>
2022-03-24 14:32:19 -0400

    style cards in impact section

--- src/components/Impact.js
+++ src/components/Impact.js
@@ -1,10 +1,10 @@
-import React                from 'react';
-import PropTypes  			    from 'prop-types';
-import { motion }           from 'framer-motion';
-import SectionHeading       from './section-heading/SectionHeading';
-import { sectionVariants }  from '../utils/animationVariants';
-import Ovals                from '../images/ovals-impact.svg';
-import Rectangles           from '../images/rectangles-impact.svg';
+import React from "react";
+import PropTypes from "prop-types";
+import { motion } from "framer-motion";
+import SectionHeading from "./section-heading/SectionHeading";
+import { sectionVariants } from "../utils/animationVariants";
+import Ovals from "../images/ovals-impact.svg";
+import Rectangles from "../images/rectangles-impact.svg";
 
 /**
  * The **Impact** component highlights the impacts for businesses and users.
@@ -12,14 +12,24 @@ import Rectangles           from '../images/rectangles-impact.svg';
  * @version 1.0.0
  * @author [Ekaterina Cratcha](https://github.com/cratcha)
  */
-function Impact({impacts}) {
-  const renderCards = () => (
-    impacts.map((cardData, index) => (
-        <article className="mt-[60px] flex flex-col items-center
-        text-center max-w-[660px] shadow-sm bg-secondary-100 z-20" key={index}>
-          <div className="mb-[60px] pr-[32px] pl-[32px]">
+function Impact({ impacts }) {
+  const renderCards = () =>
+    impacts.map((cardData, index, id) => (
+      <>
+        <div
+          key={id}
+          className=" sm:grid-cols-1 sm:gap-[520px] place-items-center translate-y-[100px] sm:translate-y-[740px] z-10"
+        >
+          {cardData.image}
+        </div>
+        <article
+          className="mt-[60px] flex flex-col items-center
+        text-center max-w-[660px] shadow-sm bg-secondary-100 z-20"
+          key={index}
+        >
+          <div className="mb-[60px] sm:mb-[40px] pr-[32px] pl-[32px]">
             {/* Card Header */}
-            <h2 className="text-lg pt-10 text-highlight-100">
+            <h2 className="text-lg pt-10 sm:pt-[30px] text-highlight-100">
               {cardData.cardHeader}
             </h2>
             {/* Card Content */}
@@ -31,7 +41,8 @@ function Impact({impacts}) {
             ))}
           </div>
         </article>
-      )));
+      </>
+    ));
 
   return (
     <motion.section
@@ -39,17 +50,18 @@ function Impact({impacts}) {
       variants={sectionVariants}
       initial="offscreen"
       whileInView="onscreen"
-      viewport={{ once: true, amount: 0.2}}>
+      viewport={{ once: true, amount: 0.2 }}
+    >
       <div className=" bg-secondary-200 pt-[120px] mx-auto flex flex-col items-center">
         <SectionHeading align="center" color="primary">
           Impact
         </SectionHeading>
 
-        <div className="grid grid-cols-2 gap-[440px] place-items-center translate-y-[100px] z-10">
+        {/* <div className="grid grid-cols-2 sm:grid-cols-1 gap-[440px] sm:gap-[520px] place-items-center translate-y-[100px] sm:translate-y-[740px] z-10">
           <img src={Rectangles} alt="" />
           <img src={Ovals} alt="" />
-        </div>
-        <div className="grid grid-cols-2 gap-[40px] justify-items-center mx-auto">
+        </div> */}
+        <div className="grid grid-cols-2 sm:grid-cols-1 gap-[40px] sm:gap-[117px] justify-items-center mx-auto">
           {renderCards()}
         </div>
       </div>
@@ -58,7 +70,7 @@ function Impact({impacts}) {
 }
 
 Impact.propTypes = {
-  impacts:    PropTypes.array.isRequired,
+  impacts: PropTypes.array.isRequired,
 };
 
 export default Impact;
--- src/constants/impacts.js
+++ src/constants/impacts.js
@@ -1,5 +1,9 @@
+import Ovals from "../images/ovals-impact.svg";
+import Rectangles from "../images/rectangles-impact.svg";
+
 const impacts = [
   {
+    image: <img src={Rectangles} alt="" />,
     cardHeader: "For Businesses",
     cardContent: [
       "Save at least 50% on supply cost",
@@ -9,6 +13,7 @@ const impacts = [
     ],
   },
   {
+    image: <img src={Ovals} alt="" />,
     cardHeader: "For Users",
     cardContent: [
       " Remove all chemicals going into food: plastics, BPA, PFAS and other 'forever chemicals' used to hold compostable packaging together",
